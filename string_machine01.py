229251b405384eaf03bfeeb6b4efe6aae77be6ba
Caketus <maxim1404@gmail.com>
2020-08-21 14:34:21 +0200

    test git atom

--- string_machine01.py
+++ string_machine01.py
@@ -1,6 +1,6 @@
 # Write a Python program to create //
 # all possible strings by using 'a', 'e', 'i', 'o', 'u'.
-# Use the characters exactly once.
+# Use the characters exactly once..
 
 import random
 import math
