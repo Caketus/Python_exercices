ffdb94b13da21be3add71fba6b009c8f557d35ac
Caketus <maxim1404@gmail.com>
2020-08-24 01:17:54 +0200

    can't fucking do iteratives

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,7 +2,7 @@ import random
 
 # Checks the difficulty and also checks if the input is correct
 
-def nb_check(nb, nb_max):
+def nb_check(nb, nb_max, lowest_nb):
     if nb == 0:
         level = input("Choose a difficulty:\nWrite 1 for easy\nWrite 2 for medium\nWrite 3 for hard\n")
         if level.isdigit() == False:
@@ -17,18 +17,19 @@ def nb_check(nb, nb_max):
             nb = 100
         if int(level) == 3:
             nb = 1000
-     # else
-     #     if
+    # else:
+    #     if nb > nb_max or nb < lowest_nb:
+
     return nb
 
 
 
 def nb_guess():
-    max_nb = 0
-    max_nb = nb_check(max_nb, 0)
-    print("el maximumo ==", max_nb)
-    user_guess = input("Guess the random number between 1 and " + str(max_nb) + "\n")
-    user_guess = nb_check(user_guess, max_nb)
+    lowest_nb = 0
+    max_nb = nb_check(0, 0, lowest_nb)
+    user_guess = input("Guess the random number between 0 and " + str(max_nb) + " included\n")
+    lowest_nb
+    user_guess = nb_check(user_guess, max_nb, lowest_nb)
 
 
 nb_guess()
