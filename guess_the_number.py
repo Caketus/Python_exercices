c5b84455b52fafbe588257ac46c4342dced50d97
Caketus <maxim1404@gmail.com>
2020-08-24 01:29:33 +0200

    Infinite levels of difficulty added (and fucking did the recursivem yeay)

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,20 +3,16 @@ import random
 # Checks the difficulty and also checks if the input is correct
 
 def nb_check(nb, nb_max, lowest_nb):
+    x = 0
     if nb == 0:
-        level = input("Choose a difficulty:\nWrite 1 for easy\nWrite 2 for medium\nWrite 3 for hard\n")
+        level = input("Choose a difficulty:\nWrite 1 for easy\nWrite 2 for medium\nWrite 3 for hard\n(and so on, there are an infinite amount of levels)\n")
         if level.isdigit() == False:
-            while level.isdigit() == False:
-                level = input("Wrong entry, try again\n")
-        if int(level) < 1 or int(level) > 3:
-            while int(level) < 1 or int(level) > 3:
-                level = input("Wrong entry, try again\n")
-        if int(level) == 1:
-            nb = 10
-        if int(level) == 2:
-            nb = 100
-        if int(level) == 3:
-            nb = 1000
+            print("Wrong entry, try again")
+            nb_check(nb, nb_max, lowest_nb)
+        nb = 1
+        while x <= int(level):
+            x += 1
+            nb *= 10
     # else:
     #     if nb > nb_max or nb < lowest_nb:
 
@@ -25,10 +21,9 @@ def nb_check(nb, nb_max, lowest_nb):
 
 
 def nb_guess():
-    lowest_nb = 0
-    max_nb = nb_check(0, 0, lowest_nb)
+    max_nb = nb_check(0, 0, 0)
     user_guess = input("Guess the random number between 0 and " + str(max_nb) + " included\n")
-    lowest_nb
+    lowest_nb = user_guess
     user_guess = nb_check(user_guess, max_nb, lowest_nb)
 
 
