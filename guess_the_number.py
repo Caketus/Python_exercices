94ec475fa774560c9775827b066ed2e8f9b018fb
Caketus <maxim1404@gmail.com>
2020-08-24 00:28:05 +0200

    Changed recursives to iteratives cuz it somehoz doesnt work ????

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,38 +1,38 @@
 import random
 
-def difficulty_check(max_nb):
-    level = input("Choose a difficulty:\nWrite 1 for easy\nWrite 2 for medium\nWrite 3 for hard\n")
-    print("level ==", level)
-    if level.isdigit() == False:
-        print("Wrong entry, try again\n")
-        if max_nb == '\0':
-            difficulty_check()
-    if int(level) < 1 or int(level) > 3:
-        print("Wrong entry, try again\n")
-        if max_nb == '\0':
-            difficulty_check()
-    if level == "1":
-        level = 10
-    if level == "2":
-        level = 100
-    if level == "3":
-        level = 1000
-    return level
+# Checks the difficulty and also checks if the input is correct
+
+def nb_check(nb, nb_max):
+    if nb == 0:
+        level = input("Choose a difficulty:\nWrite 1 for easy\nWrite 2 for medium\nWrite 3 for hard\n")
+        if level.isdigit() == False:
+            while level.isdigit() == False:
+                level = input("Wrong entry, try again\n")
+        if int(level) < 1 or int(level) > 3:
+            while int(level) < 1 or int(level) > 3:
+                level = input("Wrong entry, try again\n")
+        if int(level) == 1:
+            nb = 10
+        if int(level) == 2:
+            nb = 100
+        if int(level) == 3:
+            nb = 1000
+     # else
+     #     if
+    return nb
 
 
 
 def nb_guess():
-    max_nb = int(difficulty_check(max_nb))
-    user_gess = input("Guess the random number between 1 and ", max_nb)
+    max_nb = 0
+    max_nb = nb_check(max_nb, 0)
+    print("el maximumo ==", max_nb)
+    user_guess = input("Guess the random number between 1 and " + str(max_nb) + "\n")
+    user_guess = nb_check(user_guess, max_nb)
+
 
 nb_guess()
 
 
 
-# jvm les amis
-# jvm les amis
-# jvm les amis
-# jvm les amis
-# jvm les amis
-# jvm les amis
-# jvm les amis
+# BUVER DE L'O LES AMIS
