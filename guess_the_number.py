5a7e7f415b29e0ea4bf81463246b1848dabb0292
Caketus <maxim1404@gmail.com>
2020-08-24 18:22:19 +0200

    Done and working. Added clear of terminal and replay option

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,13 +1,16 @@
 from random import randrange
+import os
 
-# Checks the difficulty and also checks if the input is correct
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
-def difficulty_check(nb_max, lowest_nb):
+def difficulty_check():
     x = 0
+    cls()
     level = input("Choose a difficulty:\nWrite 1 for easy\nWrite 2 for medium\nWrite 3 for hard\n(and so on, there are an infinite amount of levels)\n")
     if level.isdigit() == False:
         print("Wrong entry, try again")
-        difficulty_check(nb_max, lowest_nb)
+        difficulty_check()
     nb = 1
     while x + 1 <= int(level):
         x += 1
@@ -17,16 +20,19 @@ def difficulty_check(nb_max, lowest_nb):
 def user_nb_check(user_guess, max_nb, lowest_nb):
     user_guess = input("Guess the random number between " + str(lowest_nb) + " and " + str(max_nb) + "\n")
     if user_guess.isdigit() == False:
-        print("Wrong entry, try again, put a number instead")
+        cls()
+        print("Wrong entry, try again and put a number instead")
         user_guess = user_nb_check(user_guess, max_nb, lowest_nb)
-        print("coucou")
     elif (int(user_guess) > max_nb or int(user_guess) < lowest_nb):
-        print("Wrong entry, try again")
-        user_nb_check(user_guess, max_nb, lowest_nb)
+        cls()
+        print("Wrong entry, try again and stay in range")
+        user_guess = user_nb_check(user_guess, max_nb, lowest_nb)
+    cls()
     return (int(user_guess))
 
 def nb_guess():
-    max_nb = difficulty_check(0, 0)
+    max_nb = difficulty_check()
+    cls()
     lowest_nb = 0
     user_guess = 0
     rand_nb = randrange(max_nb)
@@ -38,6 +44,9 @@ def nb_guess():
         if rand_nb < user_guess:
             print("It's lower !")
             max_nb = user_guess
-    print("You guessed the correct number ! It was " + str(user_guess))
-
+    cls()
+    if input("You guessed the correct number ! It was " + str(user_guess) + "\nDo you want to play again ?[yes/no]\n") == "yes":
+        cls()
+        nb_guess()
+    return
 nb_guess()
