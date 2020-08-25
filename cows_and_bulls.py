00fc48d8200293de2366f4db1d46ddd21d09eeb4
Caketus <maxim1404@gmail.com>
2020-08-25 22:44:17 +0200

    Working but problems when occurence of same digits, and adds +1 to both cows and bulls when there is a cow

--- cows_and_bulls.py
+++ cows_and_bulls.py
@@ -19,15 +19,35 @@ def verify_entry():
         print("We need EXACTLY 4 digits !")
         user_digits = verify_entry()
     if user_digits.isdigit() == False:
-        print("I SAID DIGIT ! :\n")
+        print("I SAID DIGITS ! :\n")
         user_digits = verify_entry()
     return user_digits
+
+def compare_digits(user_digits, solution):
+    y = 0
+    cows = 0
+    bulls = 0
+    while y <= len(solution) - 1:
+        x = 0
+        while x <= len(user_digits) - 1:
+            if solution[y] == user_digits[x]:
+                bulls += 1
+            x += 1
+        y += 1
+    x = 0
+    while x <= len(solution) - 1:
+        if solution[x] == user_digits[x]:
+            cows += 1
+        x += 1
+    return (cows, bulls)
 
 def cows_and_bulls():
     user_digits = verify_entry()
     solution = format(random.randint(0000,9999), '04d')
+    cows, bulls = compare_digits(user_digits, solution)
     print("Solution = ", solution)
-    print("Entry = ", user_digits)
+    print("cows ==", cows)
+    print("bulls ==", bulls)
 
 
 cows_and_bulls()
